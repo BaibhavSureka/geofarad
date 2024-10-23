a9c7bc8f0d7b3853a31792cce1baa56d26080f3b
Baibhav Sureka <83425243+BaibhavSureka@users.noreply.github.com>
2024-10-23 21:17:49 +0530

    update changes

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -7,19 +7,21 @@ const AboutUs = () => {
       <h2 className="about-section-title">About Us</h2>
       <div className="about-section">
         <div className="images-section">
-          <div className="image-row">
-            <img src="./assets/rectangle-2928-2.svg" alt="Image 1" className="image-1" />
-            <img src="./assets/rectangle-2818.svg" alt="Image 2" className="image-2" />
-          </div>
-          <div className="image-row">
-            <img src="./assets/rectangle-2819.svg" alt="Image 3" className="image-3" />
-            <img src="./assets/rectangle-2817.svg" alt="Image 4" className="image-4" />
+          <div className="image-grid">
+            <img src="./assets/rectangle-2928-2.svg" alt="Image 1" className="image-item" />
+            <img src="./assets/rectangle-2818.svg" alt="Image 2" className="image-item" />
+            <img src="./assets/rectangle-2819.svg" alt="Image 3" className="image-item" />
+            <img src="./assets/rectangle-2817.svg" alt="Image 4" className="image-item" />
           </div>
         </div>
         <div className="text-section">
           <h1 className="headline">We’re Creating Only Exceptional Quality Capacitors</h1>
           <p className="description">
             V.J. Capacitors Pvt Ltd, operating under the esteemed brand name GEOFARAD, specializes in manufacturing a diverse range of AC capacitors. Our advanced production facility in Noida, Uttar Pradesh, features state-of-the-art, fully automated machinery, ensuring stringent quality control. With a team of seasoned marketing professionals and engineers well-versed in the latest technology, we deliver top-notch products known for their reliability and efficiency.
+          </p>
+          <p className="vision">
+            <strong>Vision :</strong><br />
+            Our vision is to be a global leader in capacitor technology, delivering innovative and sustainable solutions to set the standard in the industry through continuous improvement and a commitment to excellence.
           </p>
           <div className="founder-section">
             <img src="./assets/ellipse-1166.svg" alt="Founder" className="founder-image" />
@@ -28,7 +30,7 @@ const AboutUs = () => {
               <p>Founder</p>
             </div>
           </div>
-          <button className="read-more-button">Read More</button>
+          <button className="read-more-button">Read Less</button>
         </div>
       </div>
     </>
