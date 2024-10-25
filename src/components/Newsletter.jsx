766ca01518cf4eff11839967e4ed201e1968424f
Baibhav Sureka <83425243+BaibhavSureka@users.noreply.github.com>
2024-10-25 17:47:01 +0530

    push

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import Form from './Form';
-// import OurBrands from './OurBrands';
+import OurBrands from './OurBrands';
 
 const Newsletter = () => {
   return (
     <>
-      {/* <OurBrands /> */}
+      <OurBrands />
       <Form />
       <div className="relative bg-red-500 text-white py-12 text-center w-full overflow-hidden"> 
         <img 
